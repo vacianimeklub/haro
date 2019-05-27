c1f100543d5375221c9326d2417a80487bbb8f36
Zoltán Adamek <zoltan.adamek@prezi.com>
2019-05-27 10:31:26 +0200

    First scratch for voting

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 
 from sqlalchemy.sql import func
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from .helpers import admin_only
+from .helpers import admin_only, build_menu
 
 from models import session
 from models.user_activity import UserActivity
@@ -20,6 +21,31 @@ def dump(bot, update):
     bot.send_document(chat_id=update.message.chat_id, document=open(SQLITE_PATH, 'rb'))
 
 
+@admin_only
+def vote(bot, update):
+    button_list = [
+        InlineKeyboardButton("Alma", callback_data=u'alma'),
+        InlineKeyboardButton("Körte", callback_data=u'körte'),
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+    bot.send_message(chat_id=update.message.chat_id, text="Szavazzunk! Szavazzunk!", reply_markup=reply_markup)
+    # switch_inline_query is going to be necessary for the last message which will publish the vote
+
+def vote_callback(bot, update):
+    bot.answer_callback_query(update.callback_query.id, text='Vettem! Vettem!')
+    button_list = [
+        InlineKeyboardButton("Alma ✅" if update.callback_query.data == u'alma' else "Alma", callback_data=u'alma'),
+        InlineKeyboardButton("Körte ✅" if update.callback_query.data == u'körte' else "Körte", callback_data=u'körte'),
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
+    origin_message_from_bot = update.callback_query.message
+    bot.edit_message_text(
+        text=u"Azt választottad, hogy '{}'!".format(update.callback_query.data),
+        chat_id=origin_message_from_bot.chat_id,
+        message_id=origin_message_from_bot.message_id,
+        reply_markup=reply_markup
+    )
+
+
 def last_message(bot, update):
     q = session.query(UserActivity).join("user").join("chat").group_by(User.id)
-
--- haro.py
+++ haro.py
@@ -2,9 +2,15 @@
 
 import logging
 
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import (
+    CallbackQueryHandler, 
+    CommandHandler, 
+    Filters, 
+    MessageHandler, 
+    Updater,
+)
 
-from handlers.command_handlers import start, dump, last_message
+from handlers.command_handlers import start, dump, last_message, vote, vote_callback
 from handlers.message_handlers import stats
 from settings import BOT_TOKEN
 
@@ -18,12 +24,15 @@ def setup_handlers(dispatcher):
     start_handler = CommandHandler('start', start)
     dump_handler = CommandHandler('dump', dump)
     last_message_handler = CommandHandler('last', last_message)
+    vote_handler = CommandHandler('vote', vote)
     stats_handler = MessageHandler(Filters.text, stats)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(dump_handler)
     dispatcher.add_handler(last_message_handler)
     dispatcher.add_handler(stats_handler)
+    dispatcher.add_handler(vote_handler)
+    dispatcher.add_handler(CallbackQueryHandler(callback=vote_callback))
 
 
 if __name__ == '__main__':
@@ -33,4 +42,3 @@ if __name__ == '__main__':
     setup_handlers(updater.dispatcher)
     updater.start_polling()
     updater.idle()
-    
