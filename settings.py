e1824b0b5bd200eba1bcd9108e8a958d769baa31
Zoltán Adamek <djscorchio@gmail.com>
2019-05-28 22:43:25 +0200

    Makes sure tests won't die without config env vars

--- settings.py
+++ settings.py
@@ -2,8 +2,13 @@ import os
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-ADMIN_USER_ID = int(os.getenv('ADMIN_USER_ID'))
-LIST_OF_ADMINS = [ADMIN_USER_ID]
+admin_user_id_raw = os.getenv('ADMIN_USER_ID')
+if admin_user_id_raw:
+    ADMIN_USER_ID = int(admin_user_id_raw)
+    LIST_OF_ADMINS = [ADMIN_USER_ID]
+else:
+    ADMIN_USER_ID = None
+    LIST_OF_ADMINS = []
 
 SQLITE_PATH = 'haro.sqlite'
 SQLALCHEMY_SQLITE_PATH = 'sqlite:///' + SQLITE_PATH
