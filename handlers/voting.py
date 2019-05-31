6533dbeaec309db73b4ebd225dc384e56fd1f0e7
Zoltán Adamek <zoltan.adamek@prezi.com>
2019-05-31 09:51:43 +0200

    Passes around chat data to keep the user's answers

--- handlers/voting.py
+++ handlers/voting.py
@@ -26,21 +26,33 @@ MULTIPLE_CHOICE = u'Lehessen többet is kiválasztani'
 def get_setup_voting_conversation_handler():
     return ConversationHandler(
         entry_points=[
-            CommandHandler('szavazas', vote_setup_start),
+            CommandHandler('szavazas', vote_setup_start, pass_chat_data=True),
         ],
 
         states={
             STATE__GET_TITLE: [
-                MessageHandler(Filters.text, vote_setup_process_title_response),
+                MessageHandler(
+                    Filters.text,
+                    vote_setup_process_title_response,
+                    pass_chat_data=True
+                ),
             ],
 
             STATE__GET_DESCRIPTION: [
-                MessageHandler(Filters.text, vote_setup_process_description_response),
+                MessageHandler(
+                    Filters.text,
+                    vote_setup_process_description_response,
+                    pass_chat_data=True
+                ),
                 # CommandHandler('skip', skip_description),
             ],
 
             STATE__GET_ANSWERS: [
-                MessageHandler(Filters.text, vote_setup_process_answers),
+                MessageHandler(
+                    Filters.text,
+                    vote_setup_process_answers,
+                    pass_chat_data=True
+                ),
             ],
 
             STATE__GET_SELECTION_MODE: [
@@ -50,31 +62,35 @@ def get_setup_voting_conversation_handler():
                         multiple=MULTIPLE_CHOICE
                     ),
                     vote_setup_process_mode_selection,
+                    pass_chat_data=True
                 ),
             ],
         },
 
         fallbacks=[
-            CommandHandler('megsem', cancel_setup_voting),
+            CommandHandler('megsem', cancel_setup_voting, pass_chat_data=True),
         ]
     )
 
 
-def vote_setup_start(bot, update):
+def vote_setup_start(bot, update, chat_data):
+    chat_data['voting'] = {}
     logger.info(u'voting starts')
     update.message.reply_text(u'Csináljunk egy szavazást! Mi legyen a címe?')
 
     return STATE__GET_TITLE
 
 
-def vote_setup_process_title_response(bot, update):
+def vote_setup_process_title_response(bot, update, chat_data):
+    chat_data['voting']['title'] = update.message.text
     logger.info(u'got title response: {}'.format(update.message.text))
     update.message.reply_text(u'Okés. Mi legyen a leírása?')
 
     return STATE__GET_DESCRIPTION
 
 
-def vote_setup_process_description_response(bot, update):
+def vote_setup_process_description_response(bot, update, chat_data):
+    chat_data['voting']['description'] = update.message.text
     logger.info(u'got description response: {}'.format(update.message.text))
     update.message.reply_text(
         u'Milyen válaszok legyenek? '
@@ -84,7 +100,7 @@ def vote_setup_process_description_response(bot, update):
     return STATE__GET_ANSWERS
 
 
-def vote_setup_process_answers(bot, update):
+def vote_setup_process_answers(bot, update, chat_data):
     logger.info(u'got answers response: {}'.format(update.message.text))
 
     if '||' not in update.message.text:
@@ -95,6 +111,8 @@ def vote_setup_process_answers(bot, update):
         )
         return STATE__GET_ANSWERS
 
+    chat_data['voting']['answers'] = update.message.text.split('||')
+
     reply_keyboard = [[SINGLE_CHOICE, MULTIPLE_CHOICE]]
     update.message.reply_text(
         u'Most már csak azt kell eldöntened, lehessen-e több választ megadni egyszerre.',
@@ -104,14 +122,17 @@ def vote_setup_process_answers(bot, update):
     return STATE__GET_SELECTION_MODE
 
 
-def vote_setup_process_mode_selection(bot, update):
+def vote_setup_process_mode_selection(bot, update, chat_data):
     logger.info(u'got mode selection response: {}'.format(update.message.text))
+
+    chat_data['voting']['multi'] = update.message.text == MULTIPLE_CHOICE
+
     update.message.reply_text(u'Készen is vagyunk, köszi!', reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
 
-def cancel_setup_voting(bot, update):
+def cancel_setup_voting(bot, update, chat_data):
     logger.info(u'user cancelled')
     update.message.reply_text(u'Akkor semmi :)')
 
